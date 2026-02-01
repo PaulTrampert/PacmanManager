c7207f0d5fb51c0010295eb8c0f6a1d2a06a236e
Paul Trampert <paul.trampert@ptrampert.com>
2026-01-31 21:37:16 -0500

    One more convenience method

--- PacmanManager.CliTools/CliToolRunner.cs
+++ PacmanManager.CliTools/CliToolRunner.cs
@@ -12,19 +12,26 @@ public class CliToolRunner(ICliOutputHandlerRegistry outputHandlerRegistry)
     private const int FwdBufferSize = 4096;
 
     /// <summary>
+    /// Run the given CLI tool. Returns the exit code of the tool.
+    /// </summary>
+    /// <param name="tool">Descriptor of the cli tool to run.</param>
+    /// <param name="ct">Cancellation Token</param>
+    /// <returns>Exit code of the tool</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the tool could not be executed.</exception>
+    public Task<int> RunToolAsync(ICliTool tool, CancellationToken ct = default)
+        => RunToolAsync(tool, Array.Empty<ICliOutputHandler>(), ct);
+    
+    /// <summary>
     /// Run the given CLI tool with specified output handlers. Returns the exit code of the tool. Upon return,
     /// all output handlers have completed processing.
     /// </summary>
     /// <param name="tool">Descriptor of the cli tool to run.</param>
-    /// <param name="outputHandler">Optional handler for the output of the tool.</param>
+    /// <param name="outputHandler">Handler for the output of the tool.</param>
     /// <param name="ct">Cancellation Token</param>
     /// <returns>Exit code of the tool</returns>
     /// <exception cref="InvalidOperationException">Thrown if the tool could not be executed.</exception>
-    public Task<int> RunToolAsync(ICliTool tool, ICliOutputHandler? outputHandler = null, CancellationToken ct = default)
-    {
-        var outputHandlers = outputHandler == null ? [] : (ICliOutputHandler[])[outputHandler];
-        return RunToolAsync(tool, outputHandlers, ct);
-    }
+    public Task<int> RunToolAsync(ICliTool tool, ICliOutputHandler outputHandler, CancellationToken ct = default)
+        => RunToolAsync(tool, [outputHandler], ct);
     
     /// <summary>
     /// Run the given CLI tool with specified output handlers. Returns the exit code of the tool. Upon return,
