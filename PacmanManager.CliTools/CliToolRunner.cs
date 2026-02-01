f9804484fbe5fe46f25315446b520f045d6c2257
Paul Trampert <paul.trampert@ptrampert.com>
2026-01-31 21:35:06 -0500

    Add convenience method for passing a single output handler

--- PacmanManager.CliTools/CliToolRunner.cs
+++ PacmanManager.CliTools/CliToolRunner.cs
@@ -16,13 +16,28 @@ public class CliToolRunner(ICliOutputHandlerRegistry outputHandlerRegistry)
     /// all output handlers have completed processing.
     /// </summary>
     /// <param name="tool">Descriptor of the cli tool to run.</param>
+    /// <param name="outputHandler">Optional handler for the output of the tool.</param>
+    /// <param name="ct">Cancellation Token</param>
+    /// <returns>Exit code of the tool</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the tool could not be executed.</exception>
+    public Task<int> RunToolAsync(ICliTool tool, ICliOutputHandler? outputHandler = null, CancellationToken ct = default)
+    {
+        var outputHandlers = outputHandler == null ? [] : (ICliOutputHandler[])[outputHandler];
+        return RunToolAsync(tool, outputHandlers, ct);
+    }
+    
+    /// <summary>
+    /// Run the given CLI tool with specified output handlers. Returns the exit code of the tool. Upon return,
+    /// all output handlers have completed processing.
+    /// </summary>
+    /// <param name="tool">Descriptor of the cli tool to run.</param>
     /// <param name="outputHandlers">List of output handlers for this specific tool run.</param>
     /// <param name="ct">Cancellation Token</param>
     /// <returns>Exit code of the tool</returns>
     /// <exception cref="InvalidOperationException">Thrown if the tool could not be executed.</exception>
     public async Task<int> RunToolAsync(
         ICliTool tool, 
-        IEnumerable<ICliOutputHandler>? outputHandlers = null,
+        IEnumerable<ICliOutputHandler> outputHandlers,
         CancellationToken ct = default
     )
     {
