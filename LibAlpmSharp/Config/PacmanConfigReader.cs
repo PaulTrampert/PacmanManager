649eca4962a8f71f0cc936878c02caf968f36ec1
Paul Trampert <paul.trampert@ptrampert.com>
2026-02-05 23:10:59 -0500

    Small refactor to PacmanConfigReader.

--- LibAlpmSharp/Config/PacmanConfigReader.cs
+++ LibAlpmSharp/Config/PacmanConfigReader.cs
@@ -11,32 +11,27 @@ public class PacmanConfigReader(ILogger<PacmanConfigReader> logger)
     public PacmanConfig ReadConfig(string filePath)
     {
         logger.LogInformation("Reading Pacman configuration from {FilePath}", filePath);
-
-        var tokenStream = new AntlrFileStream(filePath);
-        var lexer = new PacmanConfLexer(tokenStream);
-        var tokens = new CommonTokenStream(new PacmanConfigTokenSource(logger, lexer));
-        var parser = new PacmanConfParser(tokens);
-        parser.ErrorHandler = new BailErrorStrategy();
-        var errorListener = new PacmanConfigErrorListener(logger);
-        parser.AddErrorListener(errorListener);
-        var parseTree = parser.pacmanConf();
-        var visitor = new PacmanConfigVisitor(logger);
-        return visitor.Visit(parseTree);
+        var fullPath = Path.GetFullPath(filePath);
+        var tokenStream = new AntlrFileStream(fullPath);
+        return ReadConfig(tokenStream);
     }
 
-    public PacmanConfig ReadConfigFromString(string configContent, string baseDirectory)
+    public PacmanConfig ReadConfigFromString(string configContent, string fileName)
     {
-        logger.LogInformation("Reading Pacman configuration from string with base directory {BaseDirectory}", baseDirectory);
+        logger.LogInformation("Reading Pacman configuration from string using fake filename {FileName}", fileName);
 
         // Use the base directory as the "source name" for Include resolution
         var inputStream = new AntlrInputStream(configContent)
         {
-            name = baseDirectory
+            name = fileName
         };
+        return ReadConfig(inputStream);
+    }
+
+    private PacmanConfig ReadConfig(ICharStream inputStream)
+    {
         var lexer = new PacmanConfLexer(inputStream);
-        
-        var tokenSource = new PacmanConfigTokenSource(logger, lexer);
-        var tokens = new CommonTokenStream(tokenSource);
+        var tokens = new CommonTokenStream(new PacmanConfigTokenSource(logger, lexer));
         var parser = new PacmanConfParser(tokens);
         parser.ErrorHandler = new BailErrorStrategy();
         var errorListener = new PacmanConfigErrorListener(logger);
