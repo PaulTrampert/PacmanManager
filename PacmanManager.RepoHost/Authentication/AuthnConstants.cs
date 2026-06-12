d805c12cd41264d0e982be1553350d14cc624961
Paul Trampert <paul.trampert@ptrampert.com>
2026-06-11 23:49:35 -0400

    Implement ClaimsTransformer

---
+++ PacmanManager.RepoHost/Authentication/AuthnConstants.cs
@@ -0,0 +1,10 @@
+namespace PacmanManager.RepoHost.Authentication;
+
+public static class AuthnConstants
+{
+    public const string AppUserIdClaimType = "app_user_id";
+    public const string SubClaimType = "sub";
+    public const string AuthorityClaimType = "iss";
+    public const string EmailClaimType = "email";
+    public const string DisplayNameClaimType = "preferred_username";
+}
--- PacmanManager.RepoHost/Authentication/ClaimsTransformer.cs
+++ PacmanManager.RepoHost/Authentication/ClaimsTransformer.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using PacmanManager.Entities;
 using PacmanManager.RepoHost.Services;
 
 namespace PacmanManager.RepoHost.Authentication;
@@ -8,11 +9,29 @@ public class ClaimsTransformer(IUserService userService) : IClaimsTransformation
 {
     public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
-        var subject = principal.Claims.Single(c => c.Type == "sub");
-        var authority = principal.Claims.Single(c => c.Type == "iss");
-        var email = principal.Claims.Single(c => c.Type == "email");
-        var name = principal.Claims.Single(c => c.Type == "preferred_username");
+        var identity = (ClaimsIdentity)principal.Identity!;
+        if (identity.HasClaim(c => c.Type == AuthnConstants.AppUserIdClaimType))
+        {
+            return principal;
+        }
         
-        
+        var subject = principal.Claims.Single(c => c.Type == AuthnConstants.SubClaimType);
+        var authority = principal.Claims.Single(c => c.Type == AuthnConstants.AuthorityClaimType);
+        var email = principal.Claims.Single(c => c.Type == AuthnConstants.EmailClaimType);
+        var name = principal.Claims.Single(c => c.Type == AuthnConstants.DisplayNameClaimType);
+
+        var user = await userService.GetUserByExternalIdAsync(authority.Value, subject.Value);
+        if (user == null)
+        {
+            user = await userService.GetUserByEmailAsync(email.Value) ?? await userService.CreateUserAsync(new User
+            {
+                DisplayName = name.Value,
+                Email = email.Value
+            });
+            await userService.LinkToIdentityAsync(user, authority.Value, subject.Value);
+        }
+
+        identity.AddClaim(new  Claim(AuthnConstants.AppUserIdClaimType, user.Id.ToString()));
+        return principal;
     }
 }
